6617b1394f0d3069f66f9e146e2ab049741fbcaa
Nikita <nikita98.19.23@gmail.com>
2025-11-09 10:44:44 +0100

    Update SettingsPage.tsx

--- app/pages/SettingsPage.tsx
+++ app/pages/SettingsPage.tsx
@@ -1,14 +1,47 @@
-import { LargeTitle } from '../../src/components/Typography/LargeTitle/LargeTitle';
-import { Text } from '../../src/components/Typography/Text/Text';
+import { Avatar } from '../../src/components/Blocks/Avatar/Avatar';
+import { Headline } from '../../src/components/Typography/Headline/Headline';
+import { Caption } from '../../src/components/Typography/Caption/Caption';
 import { List } from '../../src/components/Blocks/List/List';
 import { Cell } from '../../src/components/Blocks/Cell/Cell';
 
 const SettingsPage = () => {
   return (
     <div className="page-content">
-      <LargeTitle weight="1" style={{ marginBottom: '20px' }}>
-        Settings
-      </LargeTitle>
+      {/* User Profile Section */}
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: '20px',
+        marginBottom: '20px',
+        marginLeft: '44px',
+        marginRight: '44px',
+      }}>
+        <Avatar
+          size={96}
+          acronym="NS"
+        />
+        <Headline 
+          weight="2" 
+          style={{ 
+            marginTop: '16px',
+            color: 'var(--tgui--text_color)',
+          }}
+        >
+          Name Surname
+        </Headline>
+        <Caption 
+          level="1"
+          style={{ 
+            marginTop: '4px',
+            color: 'var(--tgui--hint_color)',
+          }}
+        >
+          Settings
+        </Caption>
+      </div>
+
+      {/* Settings List */}
       <List>
         <Cell
           subtitle="Configure your budget preferences"
