ba7080fcdde531735083e9a5717994332529c959
Nikita <nikita98.19.23@gmail.com>
2025-11-09 10:27:29 +0100

    init 2.0

--- app/main.tsx
+++ app/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import '@telegram-apps/telegram-ui/dist/styles.css';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
