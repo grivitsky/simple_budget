ce97815d075532af5cb614971b2a7fa40bb293b2
Nikita <nikita98.19.23@gmail.com>
2025-11-11 16:45:19 +0100

    Update BudgetPage.tsx

--- app/pages/BudgetPage.tsx
+++ app/pages/BudgetPage.tsx
@@ -49,7 +49,7 @@ const BudgetPage = () => {
             letterSpacing: '-0.4px',
             color: 'var(--tgui--hint_color)',
             lineHeight: '1',
-            padding: '0px 0px 0px 6px',
+            padding: '0px 0px 0px 12px',
           }}>
             -PLN
           </span>
